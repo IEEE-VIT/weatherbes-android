a72fe44df56901c51b65ec70fa3ff68a00084162
Kajal Kumari <kajalkumari130801@gmail.com>
2021-10-03 14:44:58 +0530

    Added animated splash screen

--- app/src/main/java/com/example/weather/SplashActivity.java
+++ app/src/main/java/com/example/weather/SplashActivity.java
@@ -1,16 +1,35 @@
 package com.example.weather;
 
+import static android.view.animation.AnimationUtils.*;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    Animation topAnim, bottomAnim;
+    ImageView icon;
+    TextView name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        topAnim = loadAnimation(this,R.anim.top_animation);
+        bottomAnim = loadAnimation(this,R.anim.bottom_animation);
+
+        icon = findViewById(R.id.imageView2);
+        name = findViewById(R.id.textView);
+
+
+        icon.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
